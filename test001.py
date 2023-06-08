ffd64d0ea3b864515c31904a546355fc3e6c0224
yabool2001 <mzemlo.pl@gmail.com>
2023-06-08 18:53:11 +0200

    start

---
+++ test001.py
@@ -0,0 +1,14 @@
+import time
+# Import library
+import adi
+
+
+# Create radio object
+sdr = adi.Pluto ()
+# Configure properties
+sdr.rx_rf_bandwidth = 4000000
+# Get data
+data = sdr.rx ()
+while True :
+    print ( data )
+    time.sleep ( 3 )
